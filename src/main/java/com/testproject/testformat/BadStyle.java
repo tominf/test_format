240d6d102e10e176c3eeef30c2fdb6857c1837f5
Tomas Jirka <tomas.jirka@apris.de>
2018-10-29 11:09:43 +0100

    initial commit

---
+++ src/main/java/com/testproject/testformat/BadStyle.java
@@ -0,0 +1,12 @@
+package com.testproject.testformat;
+
+public class BadStyle {
+
+    void DoNothing() {
+/* block comment on first column - NOT to be indented */
+// line comment on first column - NOT to be indented
+        
+    /* block comment - to be indented */
+    // line comment - to be indented
+    }
+}
